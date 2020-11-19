cc746822e68c16741460a16e528ba87185d9243b
efjohannsen <efjohannsen@gmail.com>
2020-11-19 17:13:00 +0100

    HTTP request handlers

--- app.js
+++ app.js
@@ -1,16 +1,26 @@
+//importerer express library
 const express = require("express");
-const app = express(); 
+//instancierer express modulet
+const app = express();
 
 app.use(express.static("public"));
 
-
-
+//HTTP request handlers for alle endpoints som vores side skal håndtere.
 app.get("/index", (req, res) => {
     return res.sendFile(__dirname + "/public/html/index.html");
 });
 
+app.get("/page1", (req, res) => {
+    return res.sendFile(__dirname + "/public/html/page1.html");
+});
 
+app.get("/page2", (req, res) => {
+    return res.sendFile(__dirname + "/public/html/page2.html");
+});
 
+app.get("/page3", (req, res) => {
+    return res.sendFile(__dirname + "/public/html/page3.html");
+});
 
 const port = process.env.PORT || 9090; 
 
@@ -21,4 +31,4 @@ app.listen(port, (error) => {
     else{
         console.log(`Server running on port: ${port}`);
     }
-})
+});
