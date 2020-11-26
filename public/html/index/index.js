e64b4fa94ce29613b9ec3da333324875b0eaa9b8
Zamanien <tari0048@stud.kea.dk>
2020-11-26 18:07:06 +0100

    files added

---
+++ public/html/index/index.js
@@ -0,0 +1,12 @@
+function loginInput() {
+
+    const formData = document.getElementById("form-login");
+    const form = FormData(formData);
+
+    const t = forms.get("form-login");
+
+    console.log(t);
+
+    
+
+}
